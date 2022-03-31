55dd2cbc052037ba7cfb7f2d6f35c1c051a023c4
Baptiste Mnt <baptiste.menant@gmail.com>
2022-03-31 07:58:49 +0200

    Ajout fichier app.js. Récup api + récupérer les noms en fr

---
+++ app.js
@@ -0,0 +1,74 @@
+let allPokemon = [];
+let tableauFin = [];
+
+const searchInput = document.querySelector('.recherche-poke input');
+
+
+function fetchPokemonBase(){
+
+      fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+            .then(reponse => reponse.json())
+            .then((allPoke) => {
+                  // console.log(allPoke);
+                  allPoke.results.forEach((pokemon) => {
+                        fetchPokemonComplet(pokemon);
+                  })
+            })
+
+}
+fetchPokemonBase();
+
+
+function fetchPokemonComplet(pokemon) {
+      let objPokemonFull = {};
+      let url = pokemon.url;
+      let nameP = pokemon.name;
+
+      fetch(url)
+      .then(reponse => reponse.json())
+      .then((pokeData) => {
+            // console.log(pokeData);
+
+            objPokemonFull.pic = pokeData.sprites.front_default;
+            objPokemonFull.type = pokeData.types[0].type.name;
+
+            fetch(`https://pokeapi.co/api/v2/pokemon-species/${nameP}`)
+            .then(reponse => reponse.json())
+            .then((pokeData) => {
+                  // console.log(pokeData);
+
+                  objPokemonFull.name = pokeData.names[4].name;
+                  allPokemon.push(objPokemonFull);
+
+                  if(allPokemon.length === 151) {
+                        console.log(allPokemon);
+                  }
+            })
+
+      })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Animation de l'input
+
+
+searchInput.addEventListener('input', function(e) {
+
+      if(e.target.value !== "") {
+            e.target.parentNode.classList.add('active-input');
+      } else if(e.target.value === ""){
+            e.target.parentNode.classList.remove('active-input');
+      }
+
+})
